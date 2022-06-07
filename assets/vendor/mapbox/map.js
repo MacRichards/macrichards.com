5b469717597092261d6d3eb796244d4da01df0f7
Mac <macmrichards@gmail.com>
2022-06-06 20:33:18 -0700

    Update map.js

--- assets/vendor/mapbox/map.js
+++ assets/vendor/mapbox/map.js
@@ -1,7 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFjcmljaDUwMCIsImEiOiJja3lwM2gzM3MwNjdjMm5qajk1eDBzeXJmIn0.3jlV57PZfY55TULGIaRDzw';
         var map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/macrich500/cl26l0vut000014o4g60qvt3g',
             animate: true,
             center: [-122.2684, 47.6614],
             zoom: 10
