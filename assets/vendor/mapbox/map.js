8e566445ee400b1771e49a9eebe3eab26c7716fe
Mac <macmrichards@gmail.com>
2025-05-08 18:46:52 -0700

    Yearly update

--- assets/vendor/mapbox/map.js
+++
@@ -1,8 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFjcmljaDUwMCIsImEiOiJja3lwM2gzM3MwNjdjMm5qajk1eDBzeXJmIn0.3jlV57PZfY55TULGIaRDzw';
-var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v12',
-        animate: true,
-        center: [-122.1743, 47.4996],
-        zoom: 11
-});
